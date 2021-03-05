57d04e80869245dbeaeb94cced89b1c7ba82d752
David Nightingale <nightrider2552@gmail.com>
2021-03-05 13:59:38 +0300

    s

---
+++ Task1/Sort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    public static class Sort
+    {
+        public static void InsertionSort<T>(this List<T> list) where T : IComparable<T>
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                var cur = list[i];
+                var j = i;
+                while (j > 0 && cur.CompareTo(list[j - 1]) < 0)
+                {
+                    list[j] = list[j - 1];
+                    j--;
+                }
+
+                list[j] = cur;
+            }
+        }
+    }
+}
---
+++ UnitTestTask1/SortTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Task1;
+
+namespace UnitTestTask1
+{
+    [TestFixture]
+    public class SortTests
+    {
+        [Test]
+        public void InsertionSort()
+        {
+            var list = new List<int> {3, 2, 6, 9, 3, 55, 123, 32};
+            var expected = new List<int>(list);
+            list.InsertionSort();
+            expected.Sort();
+            Assert.IsTrue(list.SequenceEqual(expected));
+        }
+    }
+}
